f7f878fae477b91074065cd65c0278ec8d4b23ab
chaiqou <lomtadzenikusha@gmail.com>
2022-04-27 10:17:24 +0400

    feat: seed database using factory

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,23 @@
+<h1>{{ $heading }}</h1>
+
+@if(count($listings) === 0)
+
+<h1>No lists found</h1>
+
+@endif
+
+
+
+@foreach ($listings as $listing)
+
+<h2>
+    <a href="/listing/{{ $listing['id'] }}">
+        {{  $listing['title']}}
+    </a>
+
+</h2>
+<p>
+    {{ $listing['description'] }}
+</p>
+
+@endforeach
